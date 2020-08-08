8de54a887b29be79c94c6ea8e68bd3b09bf060ae
andrewgritten <a4gritten@gmail.com>
2020-08-08 15:34:28 -0500

    Large commit

---
+++ src/components/navigation/navigation-container.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const NavigationComponent = (props) => {
+    return (
+        <div className= "nav-wrapper">
+            <div className= "left-side">
+                <div className="nav-link-wrapper">
+                    <NavLink exact to="/" activeClassName="nav-link-active">
+                        Home
+                    </NavLink>
+                </div>
+                <div className="nav-link-wrapper">
+                    <NavLink to="/about-me" activeClassName="nav-link-active">
+                        About
+                    </NavLink>
+                </div>
+                <div className="nav-link-wrapper">
+                    <NavLink to="/contact" activeClassName="nav-link-active">
+                        Contact
+                    </NavLink>
+                </div>
+                <div className="nav-link-wrapper">
+                    <NavLink to="/blog" activeClassName="nav-link-active">
+                        Blog
+                    </NavLink>
+                </div>
+            </div>
+            <div className= "right-side">
+                ANDREW GRITTEN
+            </div>
+        </div>
+    );
+}
+
+export default NavigationComponent;
