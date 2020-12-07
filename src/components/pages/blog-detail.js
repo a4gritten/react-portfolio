156bede4f1da679a838a333e08396ee580bbdf06
andrewgritten <a4gritten@gmail.com>
2020-12-07 15:35:56 -0600

    updated dropzone component for blog form

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from "axios";
+import ReactHtmlParser from "react-html-parser";
+import BlogFeaturedImage from "../blog/blog-featured-image.js"
 
 export default class BlogDetail extends Component {
     constructor(props) {
@@ -38,17 +40,15 @@ export default class BlogDetail extends Component {
             blog_status
           } = this.state.blogItem;
       
-          return (
-            <div className="blog-container">
-              <div className="content-container">
-                <h1>{title}</h1>
+        return (
+          <div className="blog-container">
+            <div className="content-container">
+              <h1>{title}</h1>
 
-                <div className="featured-image-wrapper">
-                  <img src={featured_image_url} />  
-                </div>
-                <div className="content">
-                  {content}
-                </div>
+              <BlogFeaturedImage img={featured_image_url} />
+
+              <div className="content">{ReactHtmlParser(content)}</div>
+                {content}
               </div>
             </div>
           );
